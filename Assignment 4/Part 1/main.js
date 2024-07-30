cc781c11162cb4bec649e75f7514a90ebfe2f585
shivani3900 <shivani.patel14@dcmail.ca>
2024-07-29 22:27:03 -0400

    Assignment 4 Part 1 Sixth Commit
    I have make changes in the code.

--- Assignment 4/Part 1/main.js
+++ Assignment 4/Part 1/main.js
@@ -1,25 +1,8 @@
+const customNameInput = document.querySelector('#customname');
+const randomizeButton = document.querySelector('.randomize');
+const storyElement = document.querySelector('.story');
 
-const storyText = "Once upon a time, :insertx: went to :inserty: and found a :insertz:. The weight of the object was 300lb and the temperature was 94 degrees Fahrenheit.";
-
-const insertX = ["a dragon", "a wizard", "a knight"];
-const insertY = ["the forest", "a village", "a castle"];
-const insertZ = ["golden chalice", "magical scroll", "mysterious artifact"];
-
-function randomValueFromArray(array) {
-    return array[Math.floor(Math.random() * array.length)];
+function getRandomElement(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
 }
-function result() {
-    let newStory = storyText;
-  
-    const xItem = randomValueFromArray(insertX);
-    const yItem = randomValueFromArray(insertY);
-    const zItem = randomValueFromArray(insertZ);
-    
-    newStory = newStory.replaceAll(":insertx:", xItem)
-                       .replaceAll(":inserty:", yItem)
-                       .replaceAll(":insertz:", zItem);
-    
-    const customName = document.querySelector("#customname").value;
-    if (customName) {
-        newStory = newStory.replaceAll("Bob", customName);
-    }
