1e3eb541c30d8c8bc0d8a8c8c52e77ab3494da95
shivani3900 <shivani.patel14@dcmail.ca>
2024-07-30 17:23:43 -0400

    5th Commit Part_3
    I have added a method to draw the ball on the canvas.

--- main.js
+++ main.js
@@ -13,3 +13,10 @@ class Ball {
         this.size = size;
     }
 }
+
+draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
