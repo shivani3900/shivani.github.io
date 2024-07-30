055054d243a7d2a34b6f1e7c3558ffabb60f5cd8
shivani3900 <shivani.patel14@dcmail.ca>
2024-07-30 17:21:09 -0400

    4th Commit Part_3
    I have created a ball class.

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ const canavs = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
+
+class Ball {
+    constructor(x, y, velX, velY, color, size) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
+}
