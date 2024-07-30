d4b54a10bb1f8926596a3f6a4e51a587831e8068
shivani3900 <shivani.patel14@dcmail.ca>
2024-07-30 17:17:11 -0400

    3rd Commit Part_3
    Added another two constants for canvas for width and height.

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 const canavs = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
