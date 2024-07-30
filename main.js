1642fdbb351a091ed8e2e51b40ef9ba7f9632203
shivani3900 <shivani.patel14@dcmail.ca>
2024-07-30 18:15:12 -0400

    15th Commit Part_3
    Comments were added to some parts of the code.

--- main.js
+++ main.js
@@ -1,45 +1,52 @@
 /* 
 Name: Shivani Patel
-File: assignment4_part1
+File: assignment4_part3
 Date: 30th July, 2024
 Description: In this little balls will bounce around on the screen, and change color when they touch each other.
 */
 
-const canavs = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+// Constants for the canvas.
+const canavs = document.querySelector('canvas');// Accessing the canvas element.
+const ctx = canvas.getContext('2d');// Getting the 2D drawing context.
+const width = canvas.width = window.innerWidth;// Setting canvas width to the window width.
+const height = canvas.height = window.innerHeight;// Setting canvas height to the window height.
 
+// Defining the Ball class.
 class Ball {
     constructor(x, y, velX, velY, color, size) {
-        this.x = x;
-        this.y = y;
-        this.velX = velX;
-        this.velY = velY;
-        this.color = color;
-        this.size = size;
+        this.x = x;// Setting the x position of the ball.
+        this.y = y;// Setting the y position of the ball.
+        this.velX = velX;// Setting the x velocity of the ball.
+        this.velY = velY;// Setting the y velocity of the ball.
+        this.color = color;// Setting the color of the ball.
+        this.size = size;// Setting the size of the ball.
     }
 }
 
+// Drawing the ball on the canvas.
 draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+    ctx.beginPath();// Starting a new drawing path.
+    ctx.fillStyle = this.color;// Setting the fill color for the ball.
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);// Drawing the ball as a circle.
+    ctx.fill();// Filling the circle with the specified color.
 }
 
+// Updating the ball’s position and handling collisions with canvas edges.
 update() {
-    this.x += this.velX;
-    this.y += this.velY;
+    this.x += this.velX;// Updating the x position based on velocity.
+    this.y += this.velY;// Updating the y position based on velocity.
 
+    // Checking if the ball is hitting the right edge of the canvas.
     if (this.x + this.size > width) {
-        this.velX = -this.velX;
+        this.velX = -this.velX; // Reversing the x velocity.
       }
+       // Checking if the ball is hitting the left edge of the canvas.
       if (this.x - this.size < 0) {
-        this.velX = -this.velX;
+        this.velX = -this.velX;// Reversing the x velocity.
       }
+      // Checking if the ball is hitting the bottom edge of the canvas.
       if (this.y + this.size > height) {
-        this.velY = -this.velY;
+        this.velY = -this.velY;// Reversing the y velocity.
       }
       if (this.y - this.size < 0) {
         this.velY = -this.velY;
