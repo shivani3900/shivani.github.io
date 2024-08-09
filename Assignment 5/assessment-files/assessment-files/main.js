71c5badd9f887509ca37c2f2d4c37b98f1c647fc
shivani3900 <shivani.patel14@dcmail.ca>
2024-08-08 20:50:24 -0400

    Assignment 5- 5th commit
    In this i have added header lines

--- Assignment 5/assessment-files/assessment-files/main.js
+++ Assignment 5/assessment-files/assessment-files/main.js
@@ -1,3 +1,10 @@
+/* 
+Name: Shivani Patel
+File: assignment5
+Date: 8th August, 2024
+Description: In this, little balls will bounce around on the screen and change color when they touch each other.
+*/
+
 // functionality for showing/hiding the comments section
 
 const showHideBtn = document.querySelector('.show-hide');
